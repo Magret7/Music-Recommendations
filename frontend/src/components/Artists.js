41a7fcd5b09934bbc25ec0cf19d6a7ef6c580189
Ryan W Mullowney <rmullowney@austin.utexas.edu>
2024-03-27 17:57:21 -0500

    Make MusicRecommendations a link to homepage.  Add images to Albums, Artists, and DisplayAlbum components

--- frontend/src/components/Artists.js
+++ frontend/src/components/Artists.js
@@ -1,9 +1,9 @@
 import { Outlet, Link } from "react-router-dom";
 
 export default function Artists() {
-    const artists = [{ 'name': 'SZA', 'image': 'Place Image Here', 'info': 'Solána Imani Rowe, known professionally as SZA, is an American singer-songwriter. She first gained recognition through her self-released extended plays, See.SZA.Run and S, which helped her become the first female artist to sign with Top Dawg Entertainment.', 'tracks': ['Kill Bill', 'All the Stars', 'Snooze', 'Broken Clocks'], 'albums': ['Ctrl'], 'genres': ['R&B', 'Pop', 'Hip Hop', 'Neo Soul'], 'RelatedArtists': ['H.E.R', 'Ella Mai', 'Franck Ocean', 'Miguel', 'Jhené Aiko'] },
-    { 'name': 'J. Cole', 'image': 'Place Image Here', 'info': 'Jermaine Lamarr Cole is an American rapper and record producer. Born on a military base in Germany and raised in Fayetteville, North Carolina, Cole initially gained attention as a rapper following the release of his debut mixtape, The Come Up, in early 2007. ', 'tracks': ['She Knows', 'Power Trip', 'No Role Modelz', 'Work Out', 'Under the Sun'], 'albums': ['2014 Forest Hills Drive'], 'genres': ['Hip Hop', 'Rap'], 'RelatedArtists': ['Bas', 'Cozz', 'JID', 'Joey Badsa$$', 'Kendrick Lamar', 'JAY-Z'] },
-    { 'name': 'Ariana Grande', 'image': 'Place Image Here', 'info': 'Ariana Grande-Butera is an American singer, songwriter, and actress. Regarded as a pop culture icon, she is noted as an influential figure in popular music and as one of the most prominent vocalists of her generation, for her four-octave vocal range and signature whistle register.', 'tracks': ['7 Rings', 'Side to Side', 'thank u, next', '34+35', 'The Way'], 'albums': ['Sweetener'], 'genres': ['Pop', 'Contemporary R&B'], 'RelatedArtists': ['Doja Cat', 'SZA', 'Sabrina Carpenter', 'Victoria Monét', 'Olivia Rodrigo'] },
+    const artists = [{ 'name': 'SZA', 'image': "https://i.scdn.co/image/ab676161000051740895066d172e1f51f520bc65", 'info': 'Solána Imani Rowe, known professionally as SZA, is an American singer-songwriter. She first gained recognition through her self-released extended plays, See.SZA.Run and S, which helped her become the first female artist to sign with Top Dawg Entertainment.', 'tracks': ['Kill Bill', 'All the Stars', 'Snooze', 'Broken Clocks'], 'albums': ['Ctrl'], 'genres': ['R&B', 'Pop', 'Hip Hop', 'Neo Soul'], 'RelatedArtists': ['H.E.R', 'Ella Mai', 'Franck Ocean', 'Miguel', 'Jhené Aiko'] },
+    { 'name': 'J. Cole', 'image': "https://i.scdn.co/image/ab67616100005174add503b411a712e277895c8a", 'info': 'Jermaine Lamarr Cole is an American rapper and record producer. Born on a military base in Germany and raised in Fayetteville, North Carolina, Cole initially gained attention as a rapper following the release of his debut mixtape, The Come Up, in early 2007. ', 'tracks': ['She Knows', 'Power Trip', 'No Role Modelz', 'Work Out', 'Under the Sun'], 'albums': ['2014 Forest Hills Drive'], 'genres': ['Hip Hop', 'Rap'], 'RelatedArtists': ['Bas', 'Cozz', 'JID', 'Joey Badsa$$', 'Kendrick Lamar', 'JAY-Z'] },
+    { 'name': 'Ariana Grande', 'image': "https://i.scdn.co/image/ab6761610000517440b5c07ab77b6b1a9075fdc0", 'info': 'Ariana Grande-Butera is an American singer, songwriter, and actress. Regarded as a pop culture icon, she is noted as an influential figure in popular music and as one of the most prominent vocalists of her generation, for her four-octave vocal range and signature whistle register.', 'tracks': ['7 Rings', 'Side to Side', 'thank u, next', '34+35', 'The Way'], 'albums': ['Sweetener'], 'genres': ['Pop', 'Contemporary R&B'], 'RelatedArtists': ['Doja Cat', 'SZA', 'Sabrina Carpenter', 'Victoria Monét', 'Olivia Rodrigo'] },
     ]
 
     const artistsMap = artists.map((artist) => {
@@ -25,7 +25,7 @@ export default function Artists() {
                         </tr>
 
                         <tr>
-                            <td><b>Image: </b> {artist.image}</td>
+                            <td><img src={artist.image} alt={artist.name} className="artists--img" /></td>
                         </tr>
 
                         <tr>
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,17 +1,21 @@
+import { Outlet, Link } from "react-router-dom";
+
 export default function Header() {
     return (
         <div className="container-fluid sticky-top bg-white">
             <header className="d-flex flex-wrap align-items-center justify-content-md-between py-2 mb-4 border-bottom border-dark border-1">
                 <nav className="navbar sticky-top">
-                    {/* <div className="navbar-brand"> TODO: What was this doing before?  It doesn't have a closing tag in the header.html*/}
-                    <img
-                        src={require('../assets/images/music-icon.png')}  // TODO: Why does this need "require" to work?
-                        width="45"
-                        height="45"
-                        className="d-inline-block align-top"
-                        alt=""
-                    />{" "}
-                    <b style={{ fontSize: "1.75rem" }}> MusicRecommendations</b>
+                    <Link to={`/`} className="nav-link link-dark">
+                        {/* <div className="navbar-brand"> TODO: What was this doing before?  It doesn't have a closing tag in the header.html*/}
+                        <img
+                            src={require('../assets/images/music-icon.png')}  // TODO: Why does this need "require" to work?
+                            width="45"
+                            height="45"
+                            className="d-inline-block align-top"
+                            alt=""
+                        />{" "}
+                        <b style={{ fontSize: "1.75rem" }}> MusicRecommendations</b>
+                    </Link>
                 </nav>
 
                 {/* <form className="col-md-3 text-end" role="search">

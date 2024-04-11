af9cb4162553b09e0f86341dfcf4c4eecf58c87a
rwmullowney <rwmullowney@utexas.edu>
2024-04-10 21:57:44 -0500

    Hook Genre component up to backend data

--- frontend/src/components/Genres.js
+++ frontend/src/components/Genres.js
@@ -17,18 +17,6 @@ export default function Genres() {
     let [sliceLowerRange, sliceUpperRange] = calculateSliceRange(pageNum);
     const genreSlice = genreData.slice(sliceLowerRange, sliceUpperRange);
 
-    if (genreSlice[0]) {
-        console.log(genreSlice[0].albums);
-    }
-    // if(genreSlice[0]){console.log((JSON.parse(genreSlice[0].artists.replace("{", "[").replace("}", "]"))))}
-    // if(genreSlice[0]){newSlice = (JSON.parse(genreSlice[0].artists.replace("{", "[").replace("}", "]")))}
-    // if(genreSlice[0]){console.log(typeof(JSON.parse(genreSlice[0].artists.replace("{", "[").replace("}", "]"))))}
-
-    // console.log(newSlice)
-    // for (let i = 0; i < newSlice.length; i++) {
-    //     console.log(newSlice[i])
-    // }
-
     const genreMap = genreSlice.map((genre) => {
         return (
             <div className="col">
@@ -36,10 +24,9 @@ export default function Genres() {
                 {/* <!-- TODO: Why does this make it display well? --> */}
                 <table>
                     <tr>
-                        {/* TODO: Can this be broken out of the .map() to make this cleaner? */}
                         <th>
-                            {genre.name.charAt(0).toUpperCase() +
-                                genre.name.slice(1)}
+                            <h3>{genre.name.charAt(0).toUpperCase() +
+                                genre.name.slice(1)}</h3>
                         </th>
                     </tr>
 
@@ -51,23 +38,26 @@ export default function Genres() {
 
                     <tr>
                         <td>
-                            <b>Artists: </b>
-                            {/* {JSON.parse(genre.artists.replace("{", "[").replace("}", "]")).map(artist => <Link to={`/artist/${artist}`} style={{ marginRight: 10 }}>{artist}</Link>)} */}
+                            <b>Artists</b>
+                            <br/>
+                            {JSON.parse(genre.artists).map(artist => <Link to={`/artist/${artist}`} style={{ marginRight: 10 }}>{artist}</Link>)}
                         </td>
                     </tr>
 
                     <tr>
                         <td>
-                            <b>Albums: </b>
-                            {/* {JSON.parse(genre.albums).map(album => <Link to={`/album/${album}`} style={{ marginRight: 10 }}>{album}</Link>)} */}
+                            <b>Albums</b>
+                            <br/>
+
+                            {JSON.parse(genre.albums).map(album => <Link to={`/album/${album}`} style={{ marginRight: 10 }}>{album}</Link>)}
                         </td>
                     </tr>
 
                     <tr>
                         <td>
-                            <b>Related Songs: </b>
+                            <b>Related Tracks</b>
                             <br />
-                            {/* {JSON.parse(genre.artists.replace("{", "[").replace("}", "]")).map(track => (<>{track} <br /></>))} */}
+                            {JSON.parse(genre.tracks).map(track => (<>{track} <br /></>))}
                         </td>
                     </tr>
                 </table>
